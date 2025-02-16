1cddc9ee0ab5a848137cdad3f3fef69b4dc5fd82
Austin Okonkwo <austinokonkwo1995@gmail.com>
2025-02-16 23:10:37 +0100

    Add project files.

---
+++ CircleApp.Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CircleApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircleApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
