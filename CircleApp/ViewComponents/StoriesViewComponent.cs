443e20ec9bdc9373170c3679b184ed0b240f2447
Austin Okonkwo <austinokonkwo1995@gmail.com>
2025-02-19 21:35:47 +0100

    Added ViewComponents

---
+++ CircleApp/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircleApp.ViewComponents
+{
+    
+    public class StoriesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public StoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = await _context.Stories
+                .Where(s => s.DateCreated >= DateTime.UtcNow.AddHours(-24))
+                .Include(s => s.User)
+                .ToListAsync();
+            return View(allStories);
+        }
+
+    }
+}
