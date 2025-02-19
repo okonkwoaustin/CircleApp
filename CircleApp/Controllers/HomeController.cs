0f7a2d877242d2a7c7257114fec612afd7f7c6cf
Austin Okonkwo <austinokonkwo1995@gmail.com>
2025-02-19 23:40:25 +0100

    Added hastag on trend for you

--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using CircleApp.Data;
+using CircleApp.Data.Helpers;
 using CircleApp.Data.Models;
 using CircleApp.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
@@ -75,6 +76,33 @@ namespace CircleApp.Controllers
             //Add the post to the database
             await _context.Posts.AddAsync(newPost);
             await _context.SaveChangesAsync();
+
+            //Find and store hashtags
+            var postHashtags = HashtagsHelper.GetHashtags(post.Content);
+            foreach (var hashTag in postHashtags)
+            {
+                var hashtagDb = await _context.HashTags.FirstOrDefaultAsync(n => n.Name == hashTag);
+                if (hashtagDb != null)
+                {
+                    hashtagDb.Count += 1;
+                    hashtagDb.DateUpdated = DateTime.UtcNow;
+
+                    _context.HashTags.Update(hashtagDb);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    var newHashtag = new HashTag()
+                    {
+                        Name = hashTag,
+                        Count = 1,
+                        DateCreated = DateTime.UtcNow,
+                        DateUpdated = DateTime.UtcNow
+                    };
+                    await _context.HashTags.AddAsync(newHashtag);
+                    await _context.SaveChangesAsync();
+                }
+            }
 
             //Redirect to the index page
             return RedirectToAction("Index");
@@ -219,6 +247,21 @@ namespace CircleApp.Controllers
                 postDb.IsDeleted = true;
                 _context.Posts.Update(postDb);
                 await _context.SaveChangesAsync();
+
+                //Update hashtags
+                var postHashtags = HashtagsHelper.GetHashtags(postDb.Content);
+                foreach (var hashtag in postHashtags)
+                {
+                    var hashtagDb = await _context.HashTags.FirstOrDefaultAsync(n => n.Name == hashtag);
+                    if (hashtagDb != null)
+                    {
+                        hashtagDb.Count -= 1;
+                        hashtagDb.DateUpdated = DateTime.UtcNow;
+
+                        _context.HashTags.Update(hashtagDb);
+                        await _context.SaveChangesAsync();
+                    }
+                }
             }
 
             return RedirectToAction("Index");
---
+++ CircleApp/ViewComponents/HashtagsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircleApp.ViewComponents
+{
+    public class HashtagsViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HashtagsViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var oneWeekAgoNow = DateTime.UtcNow.AddDays(-7);
+
+            var top3Hashtags = await _context.HashTags
+                .Where(h => h.DateCreated >= oneWeekAgoNow)
+                .OrderByDescending(n => n.Count)
+                .Take(3)
+                .ToListAsync();
+
+            return View(top3Hashtags);
+        }
+    }
+}
